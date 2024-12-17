014a2c3da47a89abedeeb34218139dc68ec65ccc
Mrinali9 <139582685+Mrinali9@users.noreply.github.com>
2024-12-17 16:32:15 +0530

    Add files via upload

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,21 @@
+
+
+#ifndef PATTERNDATABASE_H
+#define PATTERNDATABASE_H
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+
+#endif //PATTERNDATABASE_H
